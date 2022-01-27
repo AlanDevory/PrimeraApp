488d6f8ac31a5f1ad4c9ce6fbb15411abcc3f203
AlanDevory <alan.gortega@alumnos.udg.mx>
2022-01-26 19:01:51 -0600

    git commit

---
+++ app/src/main/java/Gomez/Alan/primeraapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package Gomez.Alan.primeraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
